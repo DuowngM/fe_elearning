d1c3a96af9efc9fc8c155c07096907cfe0176a9e
mungvv <mung.vu@bnksolution.com>
2024-03-07 10:02:01 +0700

    update font cabin

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,21 @@ import CssBaseline from "@mui/material/CssBaseline";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Cabin, sans-serif",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-      <CssBaseline />
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>
 );
